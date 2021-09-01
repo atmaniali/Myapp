6e9762c4034c371b9bf7ae40ac68063b3d1741a9
atmaniali <atmaniali97@gmail.com>
2021-09-01 15:33:41 +0100

    add templates

--- DjangoGeo/core/views.py
+++ DjangoGeo/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index (request):
+    template_name = "core/main.html"
+    context = {}
+    return render(request, template_name, context)
